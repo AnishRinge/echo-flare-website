1e1198fb2ae527aa40062f06e0c67fd7c41faf92
Anish Mukul Ringe <crucibleanish@gmail.com>
2024-10-05 02:26:59 +0530

    Updated index.html styles.css script.js with new code and design

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// script.js
-
-// Function to show sections based on the link clicked
+// Function to show sections when clicked
 function showSection(sectionId) {
     // Hide all sections
     var sections = document.querySelectorAll('.content-section');
     sections.forEach(function(section) {
-        section.classList.add('hidden');
+        section.style.display = 'none';
     });
 
     // Show the selected section
     var selectedSection = document.getElementById(sectionId);
-    selectedSection.classList.remove('hidden');
+    selectedSection.style.display = 'block';
 }
